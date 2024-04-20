3185ebe00900f78cb630d8e254e828c78025636a
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-04-20 11:40:17 +0800

    Update dali_data.py
    恢复list_path路径

--- data/dali_data.py
+++ data/dali_data.py
@@ -14,7 +14,7 @@ class LaneExternalIterator(object):
         assert mode in ['train', 'test']
         self.mode = mode
         self.path = path
-        self.list_path = os.path.join(list_path, 'train_gt.txt')
+        self.list_path = list_path
         self.batch_size = batch_size
         self.shard_id = shard_id
         self.num_shards = num_shards
