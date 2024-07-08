b76c32bf068749d39134cefd7b0cd9c854de26fb
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-08 13:28:54 +0800

    增加dcn

--- model/backbone.py
+++ model/backbone.py
@@ -1,6 +1,9 @@
 import torch,pdb
 import torchvision
 import torch.nn.modules
+from torch import nn
+import numpy as np
+from torch.autograd import Variable
 
 class vgg16bn(torch.nn.Module):
     def __init__(self,pretrained = False):
@@ -11,27 +14,187 @@ class vgg16bn(torch.nn.Module):
         
     def forward(self,x):
         return self.model(x)
+        
+#这是不包含归一化和激活的单纯的可变形卷积
+class DeformConv2D(nn.Module):
+    def __init__(self, inc, outc, kernel_size=3, padding=1, bias=None):
+        super(DeformConv2D, self).__init__()
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.zero_padding = nn.ZeroPad2d(padding)
+        self.conv_kernel = nn.Conv2d(inc, outc, kernel_size=kernel_size, stride=kernel_size, bias=bias)
+
+    def forward(self, x, offset):
+        dtype = offset.data.type()
+        ks = self.kernel_size
+        N = offset.size(1) // 2
+
+        # Change offset's order from [x1, x2, ..., y1, y2, ...] to [x1, y1, x2, y2, ...]
+        # Codes below are written to make sure same results of MXNet implementation.
+        # You can remove them, and it won't influence the module's performance.
+        offsets_index = Variable(torch.cat([torch.arange(0, 2*N, 2), torch.arange(1, 2*N+1, 2)]), requires_grad=False).type_as(x).long()
+        offsets_index = offsets_index.unsqueeze(dim=0).unsqueeze(dim=-1).unsqueeze(dim=-1).expand(*offset.size())
+        offset = torch.gather(offset, dim=1, index=offsets_index)
+        # ------------------------------------------------------------------------
+
+        if self.padding:
+            x = self.zero_padding(x)
+
+        # (b, 2N, h, w)
+        p = self._get_p(offset, dtype)
+
+        # (b, h, w, 2N)
+        p = p.contiguous().permute(0, 2, 3, 1)
+        q_lt = Variable(p.data, requires_grad=False).floor()
+        q_rb = q_lt + 1
+
+        q_lt = torch.cat([torch.clamp(q_lt[..., :N], 0, x.size(2)-1), torch.clamp(q_lt[..., N:], 0, x.size(3)-1)], dim=-1).long()
+        q_rb = torch.cat([torch.clamp(q_rb[..., :N], 0, x.size(2)-1), torch.clamp(q_rb[..., N:], 0, x.size(3)-1)], dim=-1).long()
+        q_lb = torch.cat([q_lt[..., :N], q_rb[..., N:]], -1)
+        q_rt = torch.cat([q_rb[..., :N], q_lt[..., N:]], -1)
+
+        # (b, h, w, N)
+        mask = torch.cat([p[..., :N].lt(self.padding)+p[..., :N].gt(x.size(2)-1-self.padding),
+                          p[..., N:].lt(self.padding)+p[..., N:].gt(x.size(3)-1-self.padding)], dim=-1).type_as(p)
+        mask = mask.detach()
+        floor_p = p - (p - torch.floor(p))
+        p = p*(1-mask) + floor_p*mask
+        p = torch.cat([torch.clamp(p[..., :N], 0, x.size(2)-1), torch.clamp(p[..., N:], 0, x.size(3)-1)], dim=-1)
+
+        # bilinear kernel (b, h, w, N)
+        g_lt = (1 + (q_lt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_lt[..., N:].type_as(p) - p[..., N:]))
+        g_rb = (1 - (q_rb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_rb[..., N:].type_as(p) - p[..., N:]))
+        g_lb = (1 + (q_lb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_lb[..., N:].type_as(p) - p[..., N:]))
+        g_rt = (1 - (q_rt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_rt[..., N:].type_as(p) - p[..., N:]))
+
+        # (b, c, h, w, N)
+        x_q_lt = self._get_x_q(x, q_lt, N)
+        x_q_rb = self._get_x_q(x, q_rb, N)
+        x_q_lb = self._get_x_q(x, q_lb, N)
+        x_q_rt = self._get_x_q(x, q_rt, N)
+
+        # (b, c, h, w, N)
+        x_offset = g_lt.unsqueeze(dim=1) * x_q_lt + \
+                   g_rb.unsqueeze(dim=1) * x_q_rb + \
+                   g_lb.unsqueeze(dim=1) * x_q_lb + \
+                   g_rt.unsqueeze(dim=1) * x_q_rt
+
+        x_offset = self._reshape_x_offset(x_offset, ks)
+        out = self.conv_kernel(x_offset)
+
+        return out
+
+    def _get_p_n(self, N, dtype):
+        p_n_x, p_n_y = np.meshgrid(range(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1),
+                          range(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1), indexing='ij')
+        # (2N, 1)
+        p_n = np.concatenate((p_n_x.flatten(), p_n_y.flatten()))
+        p_n = np.reshape(p_n, (1, 2*N, 1, 1))
+        p_n = Variable(torch.from_numpy(p_n).type(dtype), requires_grad=False)
+
+        return p_n
+
+    @staticmethod
+    def _get_p_0(h, w, N, dtype):
+        p_0_x, p_0_y = np.meshgrid(range(1, h+1), range(1, w+1), indexing='ij')
+        p_0_x = p_0_x.flatten().reshape(1, 1, h, w).repeat(N, axis=1)
+        p_0_y = p_0_y.flatten().reshape(1, 1, h, w).repeat(N, axis=1)
+        p_0 = np.concatenate((p_0_x, p_0_y), axis=1)
+        p_0 = Variable(torch.from_numpy(p_0).type(dtype), requires_grad=False)
+
+        return p_0
+
+    def _get_p(self, offset, dtype):
+        N, h, w = offset.size(1)//2, offset.size(2), offset.size(3)
+
+        # (1, 2N, 1, 1)
+        p_n = self._get_p_n(N, dtype)
+        # (1, 2N, h, w)
+        p_0 = self._get_p_0(h, w, N, dtype)
+        p = p_0 + p_n + offset
+        return p
+
+    def _get_x_q(self, x, q, N):
+        b, h, w, _ = q.size()
+        padded_w = x.size(3)
+        c = x.size(1)
+        # (b, c, h*w)
+        x = x.contiguous().view(b, c, -1)
+
+        # (b, h, w, N)
+        index = q[..., :N]*padded_w + q[..., N:]  # offset_x*w + offset_y
+        # (b, c, h*w*N)
+        index = index.contiguous().unsqueeze(dim=1).expand(-1, c, -1, -1, -1).contiguous().view(b, c, -1)
+
+        x_offset = x.gather(dim=-1, index=index).contiguous().view(b, c, h, w, N)
+
+        return x_offset
+
+    @staticmethod
+    def _reshape_x_offset(x_offset, ks):
+        b, c, h, w, N = x_offset.size()
+        x_offset = torch.cat([x_offset[..., s:s+ks].contiguous().view(b, c, h, w*ks) for s in range(0, N, ks)], dim=-1)
+        x_offset = x_offset.contiguous().view(b, c, h*ks, w*ks)
+
+        return x_offset
+
+class DeformableResidualBlock(nn.Module):  
+  
+    def __init__(self, in_channels=512, out_channels=512, stride=1):  
+        super(DeformableResidualBlock, self).__init__()  
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)  
+        self.bn1 = nn.BatchNorm2d(out_channels)  
+        self.relu = nn.ReLU(inplace=True)  
+          
+        # 偏移量卷积层  
+        self.offsets = nn.Conv2d(out_channels, 18, kernel_size=3, stride=stride, padding=1)  
+          
+        # 可变形卷积层  
+        self.conv2 = DeformConv2D(out_channels, out_channels, kernel_size=3, padding=1)  
+        self.bn2 = nn.BatchNorm2d(out_channels)  
+          
+        self.stride = stride  
+  
+    def forward(self, x):  
+        residual = x  
+  
+        out = self.conv1(x)  
+        out = self.bn1(out)  
+        out = self.relu(out)  
+          
+        # 计算偏移量  
+        offsets = self.offsets(out)  
+          
+        # 应用可变形卷积  
+        out = self.conv2(out, offsets)  
+        out = self.bn2(out)  
+  
+  
+        out += residual  
+  
+        return out
+
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
         if layers == '18':
-            model = torchvision.models.resnet18(pretrained=pretrained)
+            model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
         elif layers == '34':
-            model = torchvision.models.resnet34(pretrained=pretrained)
+            model = torchvision.models.resnet34(weights="IMAGENET1K_V1")
         elif layers == '50':
-            model = torchvision.models.resnet50(pretrained=pretrained)
+            model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
         elif layers == '101':
-            model = torchvision.models.resnet101(pretrained=pretrained)
+            model = torchvision.models.resnet101(weights="IMAGENET1K_V1")
         elif layers == '152':
-            model = torchvision.models.resnet152(pretrained=pretrained)
+            model = torchvision.models.resnet152(weights="IMAGENET1K_V1")
         elif layers == '50next':
-            model = torchvision.models.resnext50_32x4d(pretrained=pretrained)
+            model = torchvision.models.resnext50_32x4d(weights="IMAGENET1K_V1")
         elif layers == '101next':
-            model = torchvision.models.resnext101_32x8d(pretrained=pretrained)
+            model = torchvision.models.resnext101_32x8d(weights="IMAGENET1K_V1")
         elif layers == '50wide':
-            model = torchvision.models.wide_resnet50_2(pretrained=pretrained)
+            model = torchvision.models.wide_resnet50_2(weights="IMAGENET1K_V1")
         elif layers == '101wide':
-            model = torchvision.models.wide_resnet101_2(pretrained=pretrained)
+            model = torchvision.models.wide_resnet101_2(weights="IMAGENET1K_V1")
         elif layers == '34fca':
             model = torch.hub.load('cfzd/FcaNet', 'fca34' ,pretrained=True)
         else:
@@ -44,8 +207,12 @@ class resnet(torch.nn.Module):
         self.layer1 = model.layer1
         self.layer2 = model.layer2
         self.layer3 = model.layer3
-        self.layer4 = model.layer4
+        
+        self.block1 = model.layer4[0]
+        self.block2 = DeformableResidualBlock()  
 
+        # print("检查结构：")
+        # print()
     def forward(self,x):
         x = self.conv1(x)
         x = self.bn1(x)
@@ -54,5 +221,7 @@ class resnet(torch.nn.Module):
         x = self.layer1(x)
         x2 = self.layer2(x)
         x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
-        return x2,x3,x4
+        x4 = self.block1(x3)
+        x5 = self.block2(x4)
+        #前两个输出是给分割的
+        return x2,x3,x5
