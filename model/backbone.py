316db23abcec7159d529381a32b27a2c895a3940
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-15 20:20:36 +0800

    修正导入路径

--- model/backbone.py
+++ model/backbone.py
@@ -4,7 +4,16 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-from ..efficientvit_master.efficientvit.models.efficientvit.backbone import efficientvit_backbone_b0 
+import sys
+import os
+sys.path.append('/kaggle/working/Ultra-Fast-Lane-Detection-v2/model')
+# 添加efficientvit文件所在的目录到Python路径
+efficientvit_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'efficientvit_master', 'efficientvit', 'models', 'efficientvit'))
+if efficientvit_path not in sys.path:
+    sys.path.append(efficientvit_path)
+
+# 导入函数
+from backbone import efficientvit_backbone_b0
 
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
