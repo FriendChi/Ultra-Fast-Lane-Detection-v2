7cd8d3c02dcb7a49e0006c221e3d758618bb2be9
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-20 18:51:03 +0800

    、

--- model/backbone.py
+++ model/backbone.py
@@ -4,36 +4,14 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-
+from .ffc_resnet import ffc_resnet18
 
         
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-        if layers == '18':
-            model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
-        elif layers == '34':
-            model = torchvision.models.resnet34(weights="IMAGENET1K_V1")
-        elif layers == '50':
-            model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
-        elif layers == '101':
-            model = torchvision.models.resnet101(weights="IMAGENET1K_V1")
-        elif layers == '152':
-            model = torchvision.models.resnet152(weights="IMAGENET1K_V1")
-        elif layers == '50next':
-            model = torchvision.models.resnext50_32x4d(weights="IMAGENET1K_V1")
-        elif layers == '101next':
-            model = torchvision.models.resnext101_32x8d(weights="IMAGENET1K_V1")
-        elif layers == '50wide':
-            model = torchvision.models.wide_resnet50_2(weights="IMAGENET1K_V1")
-        elif layers == '101wide':
-            model = torchvision.models.wide_resnet101_2(weights="IMAGENET1K_V1")
-        elif layers == '34fca':
-            model = torch.hub.load('cfzd/FcaNet', 'fca34' ,pretrained=True)
-        else:
-            raise NotImplementedError
+        model = ffc_resnet18(pretrained =True)
         
-
         self.conv1 = model.conv1
         self.bn1 = model.bn1
         self.relu = model.relu
@@ -44,6 +22,7 @@ class resnet(torch.nn.Module):
         self.layer4 = model.layer4
 
     def forward(self,x):
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -53,5 +32,4 @@ class resnet(torch.nn.Module):
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
 
-        
         return x2,x3,x4
