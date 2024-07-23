086ffcee47729cf18411c373784ee1fde1e8cdda
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-23 21:29:36 +0800

    去除不存在的maxpool

--- model/backbone.py
+++ model/backbone.py
@@ -18,7 +18,6 @@ class resnet(torch.nn.Module):
         self.conv1 = model.conv1
         self.bn1 = model.bn1
         self.relu = model.relu
-        self.maxpool = model.maxpool
         self.layer1 = model.layer1
         self.layer2 = model.layer2
         self.layer3 = model.layer3
@@ -28,7 +27,6 @@ class resnet(torch.nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.maxpool(x)
         x = self.layer1(x)
         x2 = self.layer2(x)
         x3 = self.layer3(x2)
