3b5e51d9c58311ca76ac246477e865a3758d5b3d
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-23 21:08:51 +0800

    。

--- model/backbone.py
+++ model/backbone.py
@@ -6,6 +6,43 @@ import numpy as np
 from torch.autograd import Variable
 
 
+import torch
+import torch.nn as nn
+
+class TBBasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(TBBasicBlock, self).__init__()
+
+        # BatchNorm에 bias가 포함되어 있으므로, conv2d는 bias=False로 설정합니다.
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=(stride, 2), padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.ConvTranspose2d(out_channels, out_channels * TBBasicBlock.expansion, kernel_size=3, stride=(1,2), padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * TBBasicBlock.expansion),
+        )
+
+        # identity mapping, input과 output의 feature map size, filter 수가 동일한 경우 사용.
+        self.shortcut = nn.Sequential()
+
+        self.relu = nn.ReLU()
+
+        # projection mapping using 1x1conv
+        if stride != 1 or in_channels != TBBasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * TBBasicBlock.expansion, kernel_size=1, stride=(stride, 1), bias=False),
+                nn.BatchNorm2d(out_channels * TBBasicBlock.expansion)
+            )
+
+    def forward(self, x):
+        try:
+            x = self.residual_function(x) + self.shortcut(x)
+        except:
+            x = self.residual_function(x) + self.shortcut(x)[:,:,:,:-1]
+        x = self.relu(x)
+        return x
+
         
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
@@ -48,7 +85,9 @@ class resnet(torch.nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+        print(x.shape)
         x = self.layer1(x)
+        print(x.shape)
         x2 = self.layer2(x)
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
