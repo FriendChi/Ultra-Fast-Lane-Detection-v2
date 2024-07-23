2284a48f48d2ff676642e94fed470981dc3a1a47
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-23 21:26:32 +0800

    去除最后的全连接层

--- model/backbone.py
+++ model/backbone.py
@@ -11,13 +11,27 @@ class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
         
-        self.model = iresnet18(
+        model = iresnet18(
         pretrained=False,
         num_classes=2,
         zero_init_residual=True)
-            
+        self.conv1 = model.conv1
+        self.bn1 = model.bn1
+        self.relu = model.relu
+        self.maxpool = model.maxpool
+        self.layer1 = model.layer1
+        self.layer2 = model.layer2
+        self.layer3 = model.layer3
+        self.layer4 = model.layer4
 
     def forward(self,x):
-        x4 = self.model(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x2 = self.layer2(x)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
         
         return None,None,x4
