22d0566551d15bb449f7499e13efd2539e77f5c0
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-24 21:25:08 +0800

    修改了第一个卷积的长宽变化

--- model/backbone.py
+++ model/backbone.py
@@ -4,40 +4,16 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-
+from .se import SE_ResNet18
 
         
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-        if layers == '18':
-            model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
-        elif layers == '34':
-            model = torchvision.models.resnet34(weights="IMAGENET1K_V1")
-        elif layers == '50':
-            model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
-        elif layers == '101':
-            model = torchvision.models.resnet101(weights="IMAGENET1K_V1")
-        elif layers == '152':
-            model = torchvision.models.resnet152(weights="IMAGENET1K_V1")
-        elif layers == '50next':
-            model = torchvision.models.resnext50_32x4d(weights="IMAGENET1K_V1")
-        elif layers == '101next':
-            model = torchvision.models.resnext101_32x8d(weights="IMAGENET1K_V1")
-        elif layers == '50wide':
-            model = torchvision.models.wide_resnet50_2(weights="IMAGENET1K_V1")
-        elif layers == '101wide':
-            model = torchvision.models.wide_resnet101_2(weights="IMAGENET1K_V1")
-        elif layers == '34fca':
-            model = torch.hub.load('cfzd/FcaNet', 'fca34' ,pretrained=True)
-        else:
-            raise NotImplementedError
-        
 
-        self.conv1 = model.conv1
+        model = SE_ResNet18()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
         self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
         self.layer1 = model.layer1
         self.layer2 = model.layer2
         self.layer3 = model.layer3
@@ -46,8 +22,6 @@ class resnet(torch.nn.Module):
     def forward(self,x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
         x = self.layer1(x)
         x2 = self.layer2(x)
         x3 = self.layer3(x2)
