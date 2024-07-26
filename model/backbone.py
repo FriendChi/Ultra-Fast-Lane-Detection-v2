e981f0e48c1e8d83ea6ac4ef4171078a01084044
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-07-26 22:20:45 +0800

    .

--- model/backbone.py
+++ model/backbone.py
@@ -4,54 +4,22 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-
+from timm import create_model
 
         
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-        if layers == '18':
-            model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
-        elif layers == '34':
-            model = torchvision.models.resnet34(weights="IMAGENET1K_V1")
-        elif layers == '50':
-            model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
-        elif layers == '101':
-            model = torchvision.models.resnet101(weights="IMAGENET1K_V1")
-        elif layers == '152':
-            model = torchvision.models.resnet152(weights="IMAGENET1K_V1")
-        elif layers == '50next':
-            model = torchvision.models.resnext50_32x4d(weights="IMAGENET1K_V1")
-        elif layers == '101next':
-            model = torchvision.models.resnext101_32x8d(weights="IMAGENET1K_V1")
-        elif layers == '50wide':
-            model = torchvision.models.wide_resnet50_2(weights="IMAGENET1K_V1")
-        elif layers == '101wide':
-            model = torchvision.models.wide_resnet101_2(weights="IMAGENET1K_V1")
-        elif layers == '34fca':
-            model = torch.hub.load('cfzd/FcaNet', 'fca34' ,pretrained=True)
-        else:
-            raise NotImplementedError
-        
+        model_name = "convnext_tiny.fb_in22k"
+        model = create_model(model_name, pretrained=True)
+        self.stem = model.stem
+        self.stages = model.stages
 
-        self.conv1 = model.conv1
-        self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
-        self.layer1 = model.layer1
-        self.layer2 = model.layer2
-        self.layer3 = model.layer3
-        self.layer4 = model.layer4
+
 
     def forward(self,x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x2 = self.layer2(x)
-        x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
+        x=self.stem(x)
+        x=self.stages(x)
 
         
-        return x2,x3,x4
+        return None,None,x
