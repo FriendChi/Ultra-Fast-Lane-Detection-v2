765062aa9c294e06b084c64d5c0a665856f934e6
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-02 20:54:49 +0800

    Update train.py

--- train.py
+++ train.py
@@ -52,8 +52,8 @@ def set_seed(seed):
     torch.cuda.manual_seed(seed) # GPU
     torch.cuda.manual_seed_all(seed) # All GPU
     os.environ['PYTHONHASHSEED'] = str(seed) # 禁止hash随机化
-    torch.backends.cudnn.deterministic = True # 确保每次返回的卷积算法是确定的
-    torch.backends.cudnn.benchmark = False # True的话会自动寻找最适合当前配置的高效算法，来达到优化运行效率的问题。False保证实验结果可复现
+    # torch.backends.cudnn.deterministic = True # 确保每次返回的卷积算法是确定的
+    # torch.backends.cudnn.benchmark = False # True的话会自动寻找最适合当前配置的高效算法，来达到优化运行效率的问题。False保证实验结果可复现
 
 if __name__ == "__main__":
 
