ef1e59be7113bbe4df24f320d6ca371948587629
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-03 21:11:36 +0800

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -4,12 +4,12 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-from .cbam import resnet18_cbam
+from .ca import resnet18_ca
 
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-        self.model = resnet18_cbam(True)
+        self.model = resnet18_ca(True)
     
     
     def forward(self,x):
