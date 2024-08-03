ce37cb2b9689b0ccd3b3de04b9857c733b9ee377
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-03 11:58:18 +0800

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -4,23 +4,13 @@ import torch.nn.modules
 from torch import nn
 import numpy as np
 from torch.autograd import Variable
-from vit_pytorch import ViT
+from .cbam import resnet18_cbam
 
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-    self.model = ViT(
-        image_size = 256,
-        patch_size = 32,
-        num_classes = 1000,
-        dim = 1024,
-        depth = 6,
-        heads = 16,
-        mlp_dim = 2048,
-        dropout = 0.1,
-        emb_dropout = 0.1
-    )
-    self.model.mlp_head = nn.Identity()
+        self.model = resnet18_cbam(True)
+    
     
     def forward(self,x):
         x = self.model(x)
