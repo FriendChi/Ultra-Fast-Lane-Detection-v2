fd336296c26c1057b0c5090388fb97d4a15fa4d7
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-04 22:52:39 +0800

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -9,18 +9,18 @@ from vit_pytorch import ViT
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
-    self.model = ViT(
-        image_size = 256,
-        patch_size = 32,
-        num_classes = 1000,
-        dim = 1024,
-        depth = 6,
-        heads = 16,
-        mlp_dim = 2048,
-        dropout = 0.1,
-        emb_dropout = 0.1
-    )
-    self.model.mlp_head = nn.Identity()
+        self.model = ViT(
+            image_size = 256,
+            patch_size = 32,
+            num_classes = 1000,
+            dim = 1024,
+            depth = 6,
+            heads = 16,
+            mlp_dim = 2048,
+            dropout = 0.1,
+            emb_dropout = 0.1
+        )
+        self.model.mlp_head = nn.Identity()
     
     def forward(self,x):
         x = self.model(x)
