9bd33679cbc1c86e73ff5e07cf53346669bcbb44
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-09 21:46:29 +0800

    Update ca.py

--- model/ca.py
+++ model/ca.py
@@ -174,9 +174,12 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1_1 = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1_2 = nn.BatchNorm2d(64)
+        self.conv1_3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1_3 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
@@ -212,9 +215,18 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
+        x = self.conv1_1(x)
+        x = self.bn1_1(x)
         x = self.relu(x)
+
+        x = self.conv1_2(x)
+        x = self.bn1_2(x)
+        x = self.relu(x)
+
+        x = self.conv1_3(x)
+        x = self.bn1_3(x)
+        x = self.relu(x)
+        
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -235,9 +247,12 @@ def resnet18_ca(pretrained=False, **kwargs):
     if pretrained:
         pretrained_state_dict = model_zoo.load_url(model_urls['resnet18'])
         now_state_dict        = model.state_dict()
-        now_state_dict.update(pretrained_state_dict)
+        # 遍历预训练模型的状态字典
+        for name, param in pretrained_state_dict.items():
+            if name in now_state_dict:
+                if now_state_dict[name].size() == param.size():
+                    now_state_dict[name].copy_(param)
         model.load_state_dict(now_state_dict)
-    return model
 
 
 def resnet34_ca(pretrained=False, **kwargs):
