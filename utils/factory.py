f1813a9be7b30263e959f83f6db6af01bb3f9449
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-15 22:04:45 +0800

    Update factory.py

--- utils/factory.py
+++ utils/factory.py
@@ -1,4 +1,4 @@
-from utils.loss import SoftmaxFocalLoss, ParsingRelationLoss, ParsingRelationDis, MeanLoss, TokenSegLoss, VarLoss, EMDLoss, RegLoss
+from utils.loss import SoftmaxFocalLoss, ParsingRelationLoss, ParsingRelationDis, MeanLoss, TokenSegLoss, VarLoss, EMDLoss, RegLoss,DynamicWeightCrossEntropyLoss
 from utils.metrics import MultiLabelAcc, AccTopk, Metric_mIoU, Mae
 from utils.dist_utils import DistSummaryWriter
 
@@ -38,7 +38,7 @@ def get_loss_dict(cfg):
     elif cfg.dataset in ['Tusimple', 'CULane']:
         loss_dict = {
             'name': ['cls_loss', 'relation_loss', 'relation_dis','cls_loss_col','cls_ext','cls_ext_col', 'mean_loss_row', 'mean_loss_col'],
-            'op': [SoftmaxFocalLoss(2, ignore_lb=-1), ParsingRelationLoss(), ParsingRelationDis(), SoftmaxFocalLoss(2, ignore_lb=-1), torch.nn.CrossEntropyLoss(),  torch.nn.CrossEntropyLoss(), MeanLoss(), MeanLoss(),],
+            'op': [SoftmaxFocalLoss(2, ignore_lb=-1), ParsingRelationLoss(), ParsingRelationDis(), SoftmaxFocalLoss(2, ignore_lb=-1), DynamicWeightCrossEntropyLoss(),  DynamicWeightCrossEntropyLoss(), MeanLoss(), MeanLoss(),],
             'weight': [1.0, cfg.sim_loss_w, cfg.shp_loss_w, 1.0, 1.0, 1.0, cfg.mean_loss_w, cfg.mean_loss_w,],
             'data_src': [('cls_out', 'cls_label'), ('cls_out',), ('cls_out',), ('cls_out_col', 'cls_label_col'), 
             ('cls_out_ext', 'cls_out_ext_label'), ('cls_out_col_ext', 'cls_out_col_ext_label') , ('cls_out', 'cls_label'),('cls_out_col', 'cls_label_col'),
