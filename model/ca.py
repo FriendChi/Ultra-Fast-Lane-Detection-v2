336e5357ca2f9cfcb08fbaeab27d73c4d56a5650
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-08-23 22:15:45 +0800

    Update ca.py

--- model/ca.py
+++ model/ca.py
@@ -49,42 +49,111 @@ class h_swish(nn.Module):
 class swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
-    
-class CoordAtt(nn.Module):
-    def __init__(self, inp, oup, groups=32):
-        super(CoordAtt, self).__init__()
+
+class CABottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5,ratio=32):  # ch_in, ch_out, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
+        # self.ca=CoordAtt(c1,c2,ratio)
         self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
-
-        mip = max(8, inp // groups)
-
-        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        mip = max(8, c1 // ratio)
+        self.conv1 = nn.Conv2d(c1, mip, kernel_size=1, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(mip)
-        self.conv2 = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        self.relu = h_swish()
-
+        self.act = h_swish()
+        self.conv_h = nn.Conv2d(mip, c2, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, c2, kernel_size=1, stride=1, padding=0)
     def forward(self, x):
-        identity = x
-        n,c,h,w = x.size()
-        x_h = self.pool_h(x)
-        x_w = self.pool_w(x).permute(0, 1, 3, 2)
-
+        x1=self.cv2(self.cv1(x))
+        n, c, h, w = x.size()
+        #c*1*W
+        x_h = self.pool_h(x1)
+        #c*H*1
+        #C*1*h
+        x_w = self.pool_w(x1).permute(0, 1, 3, 2)
         y = torch.cat([x_h, x_w], dim=2)
+        #C*1*(h+w)
         y = self.conv1(y)
         y = self.bn1(y)
-        y = self.relu(y) 
+        y = self.act(y)
         x_h, x_w = torch.split(y, [h, w], dim=2)
         x_w = x_w.permute(0, 1, 3, 2)
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+        out = x1 * a_w * a_h
+ 
+ 
+        # out=self.ca(x1)*x1
+        return x + out if self.add else out
 
-        x_h = self.conv2(x_h).sigmoid()
-        x_w = self.conv3(x_w).sigmoid()
-        x_h = x_h.expand(-1, -1, h, w)
-        x_w = x_w.expand(-1, -1, h, w)
+def autopad(k, p=None):
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k] 
+    return p
+class SiLU(nn.Module):
+    @staticmethod
+    def forward(x):
+        return x * torch.sigmoid(x)
 
-        y = identity * x_w * x_h
+def autopad(k, p=None):
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k] 
+    return p
 
-        return y
+class Conv(nn.Module):
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
+        super(Conv, self).__init__()
+        self.conv   = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn     = nn.BatchNorm2d(c2, eps=0.001, momentum=0.03)
+        self.act    = SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+
+class Bottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super(Bottleneck, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class C3(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(C3, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
+        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
+
+    def forward(self, x):
+        return self.cv3(torch.cat(
+            (
+                self.m(self.cv1(x)), 
+                self.cv2(x)
+            )
+            , dim=1))
+ 
+class C3CA(C3):
+    # C3 module with CABottleneck()
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(CABottleneck(c_, c_,shortcut) for _ in range(n)))
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -97,7 +166,7 @@ class BasicBlock(nn.Module):
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.coordatt = CoordAtt(planes, planes)
+        self.coordatt = C3CA(planes, planes)
 
         self.downsample = downsample
         self.stride = stride
@@ -233,11 +302,16 @@ def resnet18_ca(pretrained=False, **kwargs):
     """
     model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
     if pretrained:
+        # 加载预训练模型的权重
         pretrained_state_dict = model_zoo.load_url(model_urls['resnet18'])
-        now_state_dict        = model.state_dict()
+        now_state_dict = model.state_dict()
+
+        # 遍历预训练权重字典
+        pretrained_state_dict = {k: v for k, v in pretrained_state_dict.items() if k in now_state_dict and v.size() == now_state_dict[k].size()}
+
+        # 更新当前模型的权重
         now_state_dict.update(pretrained_state_dict)
         model.load_state_dict(now_state_dict)
-    return model
 
 
 def resnet34_ca(pretrained=False, **kwargs):
