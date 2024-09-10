536c69aa33709c67ccb1ad7de29a64dec056a92a
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-09-10 13:23:48 +0800

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -350,6 +350,28 @@ class EncoderConv(nn.Module):
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
+        if layers == '18':
+            model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
+        elif layers == '34':
+            model = torchvision.models.resnet34(weights="IMAGENET1K_V1")
+        elif layers == '50':
+            model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
+        elif layers == '101':
+            model = torchvision.models.resnet101(weights="IMAGENET1K_V1")
+        elif layers == '152':
+            model = torchvision.models.resnet152(weights="IMAGENET1K_V1")
+        elif layers == '50next':
+            model = torchvision.models.resnext50_32x4d(weights="IMAGENET1K_V1")
+        elif layers == '101next':
+            model = torchvision.models.resnext101_32x8d(weights="IMAGENET1K_V1")
+        elif layers == '50wide':
+            model = torchvision.models.wide_resnet50_2(weights="IMAGENET1K_V1")
+        elif layers == '101wide':
+            model = torchvision.models.wide_resnet101_2(weights="IMAGENET1K_V1")
+        elif layers == '34fca':
+            model = torch.hub.load('cfzd/FcaNet', 'fca34' ,pretrained=True)
+        else:
+            raise NotImplementedError
         self.conv0x = DSConv_pro(
             512,
             512,
@@ -362,12 +384,26 @@ class resnet(torch.nn.Module):
             3,
             1,
         )
-        self.model = resnet18_ca(True)
-    
+
+        self.conv1 = model.conv1
+        self.bn1 = model.bn1
+        self.relu = model.relu
+        self.maxpool = model.maxpool
+        self.layer1 = model.layer1
+        self.layer2 = model.layer2
+        self.layer3 = model.layer3
+        self.layer4 = model.layer4    
     
     def forward(self,x):
-        x = self.model(x)
-        _4x= self.conv0x(x)
-        _4y= self.conv0y(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x2 = self.layer2(x)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
+        _4x= self.conv0x(x4)
+        _4y= self.conv0y(x4)
 
         return None,_4x,_4y
