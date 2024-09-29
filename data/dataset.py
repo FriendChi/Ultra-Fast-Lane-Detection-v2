cbb26909bdaf2eb3a5cebdd31ebf185cfdb0443e
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-09-29 16:18:39 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -24,7 +24,9 @@ class LaneTestDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         name = self.list[index].split()[0]
-        img_path = os.path.join('/kaggle/input/tusimple/TUSimple/test_set', name)
+        print(self.path,name)
+        img_path = os.path.join(self.path, name)
+        
         img = loader_func(img_path)
 
         if self.img_transform is not None:
