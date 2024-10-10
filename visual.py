f54c55a3e788b446041e2afa60b11691e21c147b
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-10-10 15:23:27 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -23,26 +23,30 @@ img_tensor = img_tensor.unsqueeze(0)  # 增加 batch 维度，形状变为 (1, 3
 x = img_tensor.cuda()  # 移动到 GPU 上
 print('x:',x.shape)
 
-y,feature_map = net(x)
+y,feature_map,fea3,fea2,fea1 = net(x)
 
-# 计算在通道维度上的平均值，得到形状为 (1, 25, 40)
-mean_feature_map = feature_map.mean(dim=1, keepdim=True)  # 保持维度为 1
-mean_feature_map = mean_feature_map.squeeze(0).detach().cpu().numpy()  # 转换为 NumPy 数组
+l = [feature_map,fea3,fea2,fea1]
 
-# 假设你已经得到了 `heatmap_data`，它的形状为 (25, 40)
-heatmap_data = mean_feature_map.squeeze(0)  # 如果使用平均值方法
-# heatmap_data = specific_channel  # 如果选择了特定通道
+def fun(mean_feature_map,num):
+    # 计算在通道维度上的平均值，得到形状为 (1, 25, 40)
+    mean_feature_map = feature_map.mean(dim=1, keepdim=True)  # 保持维度为 1
+    mean_feature_map = mean_feature_map.squeeze(0).detach().cpu().numpy()  # 转换为 NumPy 数组
+    
+    # 假设你已经得到了 `heatmap_data`，它的形状为 (25, 40)
+    heatmap_data = mean_feature_map.squeeze(0)  # 如果使用平均值方法
+    # heatmap_data = specific_channel  # 如果选择了特定通道
+    
+    plt.figure(figsize=(8, 6))
+    plt.imshow(heatmap_data, cmap='jet', aspect='auto')  # 选择颜色映射
+    plt.colorbar()  # 显示颜色条
+    plt.title('Heatmap')
+    plt.xlabel('Width')
+    plt.ylabel('Height')
+    
+    # 保存热图
+    plt.savefig('heatmap_{num}.png', bbox_inches='tight')  # 可以指定路径和文件名
+    plt.close()  # 关闭当前图形
 
-plt.figure(figsize=(8, 6))
-plt.imshow(heatmap_data, cmap='jet', aspect='auto')  # 选择颜色映射
-plt.colorbar()  # 显示颜色条
-plt.title('Heatmap')
-plt.xlabel('Width')
-plt.ylabel('Height')
-
-# 保存热图
-plt.savefig('heatmap.png', bbox_inches='tight')  # 可以指定路径和文件名
-plt.close()  # 关闭当前图形
-
-
+for i,map in enumerate(l):
+    fun(map,i)
 
